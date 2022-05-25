12889ebfd131de9730a733f43d32bd8c28d34a4b
Sam Smith <korhan.tokgoz92@gmail.com>
2022-05-25 18:54:22 +0300

    first commit

---
+++ src/data.js
@@ -0,0 +1,32 @@
+const reviews = [
+  {
+    id: 1,
+    name: 'Mustafa Taş',
+    job: 'Singer',
+    image: '/img/indir.jpg',
+    text: "Mustafa Taş is originally from Ankara. Mustafa Taş is a person who was born and raised in Ankara to a civil servant father from Ankara, was introduced to saz at an early age, and then strives to bring useful works to the society by improving his art. Mustafa Taş's debut album, Atam Seymenler, was released in 2009.",
+  },
+  {
+    id: 2,
+    name: 'Adele',
+    job: 'Singer',
+    image: '/img/adele.jpg',
+    text: 'Adele Laurie Blue Adkins was born on 5 May 1988 to a young single mother in Tottenham Hotspur, London, England. In 2015, he released the Single "Hello". This song has been one of the songs that reached over 1 billion views on Youtube.',
+  },
+  {
+    id: 3,
+    name: 'Ömer Başdoğan',
+    job: 'Influencer',
+    image: '/img/nalet_bebe.jpg',
+    text: 'Born on March 7, 1987, Ömer Başdoğan was born in Ankara.Known as Nalet Bebe, Başdoğan became famous with the videos she posted on Instagram and YouTube platforms.',
+  },
+  {
+    id: 4,
+    name: 'Cristiano Ronaldo',
+    job: 'Footballer',
+    image: '/img/ronaldo.jpg',
+    text: 'Cristiano Ronaldo dos Santos Aveiro is a Portuguese national football player who plays in the left wing and centre-forward positions. He plays for Manchester United, one of the Premier League teams. RONALDO is the top scorer in football history. ',
+  },
+];
+
+export default reviews;
