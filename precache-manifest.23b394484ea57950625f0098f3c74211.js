84a99001991b6695ae986cbb5c048004c65ed3e8
Sam Smith <korhan.tokgoz92@gmail.com>
2022-05-25 18:57:08 +0300

    Updates

---
+++ precache-manifest.23b394484ea57950625f0098f3c74211.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09194a1d40c17cdd3c93acafe1821b15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e36e1b8f94518e6525ee",
+    "url": "/static/css/main.ec75971b.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e36e1b8f94518e6525ee",
+    "url": "/static/js/main.e3f38392.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
